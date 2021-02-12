d61e867867487e478e88c9d3a8b5d0b0825de577
Randika Nurfaizi <44109733+dikanurfaizi@users.noreply.github.com>
2021-02-12 14:19:21 +0700

    Latihan ASP NET MVC

---
+++ ASPNetMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using ASPNetMVC.Context;
+using ASPNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        private MyContext myContext = new MyContext();
+        public ActionResult Index()
+        {
+            return View(myContext.Employees.ToList());
+        }
+
+        public ActionResult Details(int Id)
+        {
+            return View(myContext.Employees.Find(Id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            myContext.Employees.Add(employee);
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return View(myContext.Employees.Find(Id));
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            myContext.Entry(employee).State = EntityState.Modified;
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            return View(myContext.Employees.Find(Id));
+        }
+        
+        [HttpPost]
+        public ActionResult Delete(Employee employee, int Id)
+        {
+            var delete = myContext.Employees.Find(Id);
+            myContext.Employees.Remove(delete);
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ASPNetMVC/Migrations/202102120715227_SecondMigrations.cs
@@ -0,0 +1,26 @@
+﻿namespace ASPNetMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SecondMigrations : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tb_Division",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tb_Division");
+        }
+    }
+}
